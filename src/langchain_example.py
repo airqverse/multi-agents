639918a5d4de707aa919b334e5b12f6ad6cd0ec0
airq <airq1106@gmail.com>
2025-07-17 11:45:08 +0900

    Added a pipe to the chain.

--- src/langchain_example.py
+++ src/langchain_example.py
@@ -8,20 +8,24 @@ def main():
         "What is a good name for a company that makes {product}?"
     )
 
+    prompt2 = ChatPromptTemplate.from_template(
+        "Make the name at least {num} words."
+    )
+
     # Initialize the OpenAI LLM
     # It uses API key defined in OPENAI_API_KEY environment variable
     llm = OpenAI(temperature=0.9)
 
     # Pipe the prompt to the llm, and then to a string output parser
-    chain = prompt | llm | StrOutputParser()
+    chain = prompt | llm | prompt2 | llm | StrOutputParser()
 
     # Define the product we want a company name for
-    product_query = "colorful socks"
+    product_query = "New age flashcard app"
 
     print(f"Generating a company name for a company that makes: {product_query}")
 
     # Run the chain with our product query as a dictionary
-    company_name = chain.invoke({"product": product_query})
+    company_name = chain.invoke({"product": product_query, "num": 5})
 
     print(f"Generated Company Name: {company_name.strip()}")
 
